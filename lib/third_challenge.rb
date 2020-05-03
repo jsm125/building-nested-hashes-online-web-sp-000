358b02f5b34c2d0c9b2c266c7b15e9104d3960d4
Matt Im <matthew.seiim@gmail.com>
2020-05-03 20:54:24 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -23,18 +23,18 @@ def third_challenge
       :patriarch => {
         #your key/value pairs here
         name: "Lord Capulet",
-        age: => "51"
+        age: "50"
         },
       :matriarch => {
         #your key/value pairs here
-        name: => "Lady Capulet",
-        age: => "51"
+        name: "Lady Capulet",
+        age: "51"
         },
       :heroine => {
         #your key/value pairs here
-        name: => "Juliet",
-        age: => "15",
-        status: => "alive"
+        name: "Juliet",
+        age: "15",
+        status: "alive"
         },
       :heroine_friends => []
    }
